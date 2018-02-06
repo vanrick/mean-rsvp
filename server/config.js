cb0992294374bb56047b93b338f45fc1562a3cc5
Rick Van <x.rckvan@gmail.com>
2018-02-06 07:39:39 -0700

    half way point

---
+++ server/config.js
@@ -0,0 +1,6 @@
+// server/config.js
+module.exports = {
+    AUTH0_DOMAIN: 'https://rsvp-mean-app.auth0.com/api/v2/', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: process.env.MONGO_URI || 'mongodb://:@ds125068.mlab.com:25068/mean'
+};
